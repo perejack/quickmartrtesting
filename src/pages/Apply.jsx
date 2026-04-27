84d2700cc2e4a522c090ce011795c2873e722994
Paytra Admin <admin@paytra.com>
2026-04-27 16:05:09 +0300

    Fix: Add missing useParams import

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import { useParams, useNavigate, Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
